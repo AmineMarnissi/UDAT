f14ecd9837a0417204e4de33c8626b0bcac4c920
Mohamed amine marnissi <35928931+AmineMarnissi@users.noreply.github.com>
2023-05-25 07:15:35 -0400

    Delete train_ps_cl.sh

--- train_ps_cl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source_dataset="voc_0712"
-target_dataset="clipart"
-net="res101"
-s=1
-gamma=3.0
-
-python train_net.py --dataset ${source_dataset} --dataset_t ${target_dataset} --net ${net} --s ${s} --epochs 8 --gamma ${gamma}
